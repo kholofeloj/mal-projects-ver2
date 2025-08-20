9f1e1d297354f709ffea4ebe605d841e1a028790
kholofeloj <kholofelojmocheko@gmail.com>
2025-08-20 13:41:06 +0200

    added php file + contact form (no styles)

---
+++ app/php/main.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $contactNumber = htmlspecialchars($_POST['contactNumber']);
+    $mailFrom = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $mailTo = "info@janmckkreatives.co.za"; 
+    $subject = "New message from: " . $name;
+    
+    // HTML email body
+    $txt = "<strong>You have received a message from: </strong>" . $name . "<br><br>";
+    $txt .= "<strong>Email: </strong>" . $mailFrom . "<br>";
+    $txt .= "<strong>Contact Number: </strong>" . $contactNumber . "<br>";
+    $txt .= "<strong>Message: </strong><br>" . nl2br($message); // Convert newlines to <br>
+
+    // Email headers
+    $headers = "From: " . $mailFrom . "\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    // Send email and handle result
+    if (mail($mailTo, $subject, $txt, $headers)) {
+        header("Location: mail.html?mailsend");  // Redirect with success message
+        exit();  // Prevent further script execution
+    } else {
+        echo "Error: Unable to send email.";
+    }
+}
+
+?>
